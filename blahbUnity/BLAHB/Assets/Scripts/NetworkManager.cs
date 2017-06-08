255cda18b3a9a3582052f2af43c7d4a4a4da45d2
Benjamin Allen <benjaminthomasallen@gmail.com>
2017-06-08 11:57:50 -0400

    Unity project w/ splash screen, main menu, and begining networking

---
+++ blahbUnity/BLAHB/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		PhotonNetwork.ConnectUsingSettings ("0.1");
+	}
+
+	private const string roomName = "RoomName";
+	private RoomInfo[] roomsList;
+
+	void OnGUI()
+	{
+		if (!PhotonNetwork.connected)
+		{
+			GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+		}
+		else if (PhotonNetwork.room == null)
+		{
+			// Create Room
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				PhotonNetwork.CreateRoom(roomName + Guid.NewGuid().ToString("N"), true, true, 5);
+
+			// Join Room
+			if (roomsList != null)
+			{
+				for (int i = 0; i < roomsList.GetLength(0); i++)
+				{
+					if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].Name))
+						PhotonNetwork.JoinRoom(roomsList[i].Name);
+				}
+			}
+		}
+	}
+
+	void OnReceivedRoomListUpdate()
+	{
+		roomsList = PhotonNetwork.GetRoomList();
+	}
+	void OnJoinedRoom()
+	{
+		Debug.Log("Connected to Room");
+	}
+		
+	/*
+	// Update is called once per frame
+	void Update () {
+		
+	}*/
+}
---
+++ blahbUnity/BLAHB/Assets/Scripts/mainMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuScript : MonoBehaviour 
+{
+
+	public void OnStartGameButton()
+	{
+		SceneManager.LoadScene ("Connection");
+	}
+
+	public void OnCreditsButton()
+	{
+		SceneManager.LoadScene ("Credits");
+	}
+
+	public void OnQuitButton()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#endif
+		Application.Quit ();
+
+	}
+		
+}
