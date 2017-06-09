0251f824fe2c9724b660a54ee5e4595bfcc5c3a8
Benjamin Allen <benthomasallen@gmail.com>
2017-06-09 13:52:10 -0400

    Additional tile functionality

--- blahbUnity/BLAHB/Assets/Scripts/GridTileScript.js
+++ blahbUnity/BLAHB/Assets/Scripts/GridTileScript.js
@@ -3,6 +3,7 @@
 public var tilePrefab: GameObject;
 public var numberOfTiles: int = 10;
 public var distanceBetweenTiles: float = 1.0;
+public var tilesPerRow: int = 4;
 
 function Start () 
 {
@@ -12,11 +13,19 @@ function Start ()
 function CreateTiles()
 {
 	var xOffset: float = 0.0;
+	var zOffset: float = 0.0;
 
 	for(var tilesCreated: int = 0; tilesCreated < numberOfTiles; tilesCreated += 1)
 	{
 		xOffset += distanceBetweenTiles;
-		Instantiate(tilePrefab, Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z), transform.rotation);
+
+		if(tilesCreated % tilesPerRow == 0)
+		{
+			zOffset += distanceBetweenTiles;
+			xOffset = 0;
+		}
+
+		Instantiate(tilePrefab, Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z + zOffset), transform.rotation);
 	}
 }
 
